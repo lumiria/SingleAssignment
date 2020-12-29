922fc91cf8a4bc456a37330060224c7167884b27
lumiria <lumiere.mail@lumiange.com>
2020-12-29 18:08:46 +0900

    Adds the GetOrSet method.

--- src/Extensions/OnceExtensions.cs
+++ src/Extensions/OnceExtensions.cs
@@ -5,6 +5,12 @@ namespace SignleAssignment.Extensions
 {
     public static class OnceExtensions
     {
+        /// <summary>
+        /// Runs a work.
+        /// </summary>
+        /// <typeparam name="T">The type of value.</typeparam>
+        /// <param name="action">The work to execute if the current <see cref="Once{T}"/> object has a value.</param>
+        /// <param name="actionIfEmpty">The work to execute if the curremt <see cref="Once{T}"/> object has not a value.</param>
         public static void Run<T>(this Once<T> self, Action<T>? action, Action? actionIfEmpty)
         {
             if (self.HasValue)
@@ -13,6 +19,22 @@ namespace SignleAssignment.Extensions
                 return;
             }
             actionIfEmpty?.Invoke();
+        }
+
+        /// <summary>
+        /// Sets a value by using the specified function if the value does not already exist. Returns the new value, or the existing value if the value exists.
+        /// </summary>
+        /// <typeparam name="T">The type of value.</typeparam>
+        /// <param name="setter">The function of the value to add.</param>
+        /// <returns>The value. This will be either the existing value if the value is already, or the new value if the value was not in myself.</returns>
+        public static T GetOrSet<T>(this Once<T> self, Func<T> valueFactory)
+        {
+            if (self.HasValue)
+            {
+                return self;
+            }
+            self.TrySet(valueFactory());
+            return self;
         }
     }
 }
--- src/Once.cs
+++ src/Once.cs
@@ -2,21 +2,39 @@
 
 namespace SignleAssignment
 {
+    /// <summary>
+    /// Represents a value type that can be assigned a value only once.
+    /// </summary>
+    /// <typeparam name="T">The underlying value type of the <see cref="Once{T}"/> generic type.</typeparam>
     public sealed class Once<T>
     {
         private T _value;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Once"/> class.
+        /// </summary>
         internal Once()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Once{T}"/> class to the specified value.
+        /// </summary>
+        /// <param name="value"></param>
         internal Once(T value)
         {
             Set(value);
         }
 
+        /// <summary>
+        /// Gets a value indicating whether the current <see cref="Once{T}"/> object has a value.
+        /// </summary>
         public bool HasValue { get; private set; }
 
+        /// <summary>
+        /// Gets the value of the current <see cref="Once{T}"/> object if it has been assigned a value.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">The <see cref="HasValue"/> property is false.</exception>
         public T Value
         {
             get => _value;
@@ -30,6 +48,11 @@ namespace SignleAssignment
             }
         }
 
+        /// <summary>
+        /// Sets the value if a value is not assigned to the current <see cref="Once{T}"/> object.
+        /// </summary>
+        /// <param name="value">The value to set.</param>
+        /// <returns>true if the current <see cref="Once{T}"/> object has not had a value; otherwise, false.</returns>
         public bool TrySet(T value)
         {
             if (HasValue)
@@ -52,9 +75,20 @@ namespace SignleAssignment
 
     public static class Once
     {
+        /// <summary>
+        /// Creates a new <see cref="Once{T}"/> object.
+        /// </summary>
+        /// <typeparam name="T">The underlying value type of the <see cref="Once{T}"/> generic type.</typeparam>
+        /// <returns></returns>
         public static Once<T> Create<T>() =>
             new Once<T>();
 
+        /// <summary>
+        /// Creates a new <see cref="Once{T}"/> object initialized to a specified value.
+        /// </summary>
+        /// <typeparam name="T">The underlying value type of the <see cref="Once{T}"/> generic type.</typeparam>
+        /// <param name="value">A value.</param>
+        /// <returns>A <see cref="Once{T}"/> object whose <see cref="Value"/> property is assigned with the value parameter.</returns>
         public static Once<T> Create<T>(T value) =>
             new Once<T>(value);
     }
